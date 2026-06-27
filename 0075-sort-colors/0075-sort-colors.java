109326195a28bc466c78e92797e19f42725897c0
Krishna jha <157403687+kri-jha@users.noreply.github.com>
2026-06-27 13:15:44 +0530

    Time: 0 ms (100%), Space: 43.6 MB (42.04%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,40 +1,82 @@
 class Solution {
     public void sortColors(int[] nums) {
-        // Arrays.sort(nums);
-        int l =0;
-        int n = nums.length;
-        int r = n-1;
-        int i =0;
+        // // Arrays.sort(nums);
+        // int l =0;
+        // int n = nums.length;
+        // int r = n-1;
+        // int i =0;
 
         
         
-            while(i<=r)
+        //     while(i<=r)
+        //     {
+        //         if(nums[i]==0)
+        //         {
+        //             int temp = nums[i];
+        //             nums[i] = nums[l];
+        //            nums[l] = temp;
+        //             l++;
+        //             i++;
+        //         } else if(nums[i] ==1)
+        //         {
+        //             i++;
+        //         }
+
+
+
+
+        //         else
+        //         {
+        //               int temp = nums[i];
+        //         nums[i] = nums[r];
+        //         nums[r] = temp;
+
+        //             r--;
+                    
+                    
+        //         }
+        //     }
+
+        int count0 = 0;
+        int count1 = 0;
+        int count2=0;
+        for(int i = 0; i<nums.length; i++)
+        {
+            if(nums[i]==0)
             {
-                if(nums[i]==0)
-                {
-                    int temp = nums[i];
-                    nums[i] = nums[l];
-                   nums[l] = temp;
-                    l++;
-                    i++;
-                } else if(nums[i] ==1)
-                {
-                    i++;
-                }
-
-
-
-
-                else
-                {
-                      int temp = nums[i];
-                nums[i] = nums[r];
-                nums[r] = temp;
-
-                    r--;
-                    
-                    
-                }
+                count0++;
+            } else if(nums[i]==1)
+            {
+                count1++;
+            } else 
+            {
+                count2++;
             }
+        }
+
+         int index = 0;
+
+        while (count0 > 0) {
+            nums[index] = 0;
+            index++;
+            count0--;
+        }
+
+        while(count1>0)
+        {
+            nums[index] =1;
+            index++;
+            count1--;
+        }
+
+
+        while (count2> 0) {
+            nums[index] = 2;
+            index++;
+            count2--;
+        }
+
+
+   
     }
 }
