88f64d6fc0fa0265ba2842d5a849a175f3b7cf9a
Krishna jha <157403687+kri-jha@users.noreply.github.com>
2026-07-13 00:48:19 +0530

    Time: 0 ms (100%), Space: 48.3 MB (37.52%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int low = 0;
+        int high = s.length-1;
+        while(low <high)
+        {
+            //swap
+            char temp = s[low];
+            s[low] = s[high];
+            s[high] = temp;
+            low++;
+            high--;
+
+        }
+      
+
+    }
+}
