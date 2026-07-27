2bd61395931c2c0cafb59eb07270abb70b99d09d
Krishna jha <157403687+kri-jha@users.noreply.github.com>
2026-07-27 18:31:31 +0530

    Time: 0 ms (100%), Space: 44.6 MB (60.68%) - LeetHub

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.java
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.java
@@ -1,44 +1,48 @@
 class Solution {
     public int maxProduct(int[] nums) {
         int n = nums.length;
-        int max =0;
+        // int max =0;
         // Arrays.sort(nums);
 
         // int i =nums[n-2];
         // int j = nums[nums.length-1];
         //  int p = ((nums[i]-1)* (nums[j]-1));
 
-    //     int fm =nums[0];
-    //     int sm =0;
-
-    //     for(int i=1; i<n; i++)
-    //     {
-    //         if(nums[i]>=fm)
-    //         {
-    //             int temp =fm;
-    //             sm = temp;
-    //             fm = nums[i];
-    //         }
-    //     }
-
-    // // int p = ((nums[fm]-1)* (nums[j]-1));
-    // int p = (fm-1)*(sm-1);
-
-    // return p;
-        
-
-        
-
+        int fm =0;
+        int sm =0;
 
         for(int i=0; i<n; i++)
         {
-            for(int j=i+1; j<n; j++)
+            if(nums[i]>fm)
             {
-                int p = ((nums[i]-1)* (nums[j]-1));
-                max = Math.max(max, p);
+                // int temp =fm;
+                sm = fm;
+                fm = nums[i];
+            } 
+            else if(nums[i]>sm)
+            {
+                sm =nums[i];
             }
         }
-        return max;
+
+    // int p = ((nums[fm]-1)* (nums[j]-1));
+    int p = (fm-1)*(sm-1);
+
+    return p;
+        
+
+        
+
+
+        // for(int i=0; i<n; i++)
+        // {
+        //     for(int j=i+1; j<n; j++)
+        //     {
+        //         int p = ((nums[i]-1)* (nums[j]-1));
+        //         max = Math.max(max, p);
+        //     }
+        // }
+        // return max;
 
         // return p;
     }
