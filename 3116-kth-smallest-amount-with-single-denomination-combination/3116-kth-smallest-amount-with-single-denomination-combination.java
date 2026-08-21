3d06c44cf9c044568a556b085ead1ef95abd465a
Krishna jha <157403687+kri-jha@users.noreply.github.com>
2026-08-22 01:30:17 +0530

    Time: 170 ms (19.23%), Space: 43.3 MB (94.23%) - LeetHub

---
+++ 3116-kth-smallest-amount-with-single-denomination-combination/3116-kth-smallest-amount-with-single-denomination-combination.java
@@ -0,0 +1,82 @@
+class Solution {
+
+    public long findKthSmallest(int[] coins, int k) {
+
+        long left = 1;
+
+        long minCoin = Integer.MAX_VALUE;
+        for (int coin : coins) {
+            minCoin = Math.min(minCoin, coin);
+        }
+
+        long right = minCoin * (long) k;
+
+        while (left < right) {
+
+            long mid = left + (right - left) / 2;
+
+            if (count(mid, coins) >= k) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+
+    private long count(long x, int[] coins) {
+
+        int n = coins.length;
+        long total = 0;
+
+        // Generate all non-empty subsets
+        for (int mask = 1; mask < (1 << n); mask++) {
+
+            long lcm = 1;
+            int bits = 0;
+
+            for (int i = 0; i < n; i++) {
+
+                if ((mask & (1 << i)) != 0) {
+
+                    bits++;
+
+                    lcm = lcm(lcm, coins[i]);
+
+                    // No multiple of this LCM <= x
+                    if (lcm > x) {
+                        break;
+                    }
+                }
+            }
+
+            long multiples = x / lcm;
+
+            // Odd number of coins -> add
+            // Even number of coins -> subtract
+            if (bits % 2 == 1) {
+                total += multiples;
+            } else {
+                total -= multiples;
+            }
+        }
+
+        return total;
+    }
+
+    private long lcm(long a, long b) {
+        return a / gcd(a, b) * b;
+    }
+
+    private long gcd(long a, long b) {
+
+        while (b != 0) {
+            long temp = a % b;
+            a = b;
+            b = temp;
+        }
+
+        return a;
+    }
+}
