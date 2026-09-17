d04142895dd70272cd291bc0bff2bd470b002408
Krishna jha <157403687+kri-jha@users.noreply.github.com>
2026-09-18 00:48:20 +0530

    Time: 27 ms (86.03%), Space: 55.3 MB (30.28%) - LeetHub

---
+++ 0901-online-stock-span/0901-online-stock-span.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class StockSpanner {
+
+    Stack<int[]> s;
+
+    public StockSpanner() {
+        s = new Stack<>();
+    }
+
+    public int next(int price) {
+
+        int span = 1;
+
+        while(!s.isEmpty() && s.peek()[0] <= price) {
+            span += s.pop()[1];
+        }
+
+        s.push(new int[]{price, span});
+
+        return span;
+    }
+}
